186028292bc7b34a2d4ea1a1f509c1164276b2b6
Dunesword <157728274+Dunesword@users.noreply.github.com>
2026-06-08 21:00:45 -0400

    fixed crusher sfx null error

--- Assets/Scripts/Crusher.cs
+++ Assets/Scripts/Crusher.cs
@@ -11,6 +11,11 @@ public class Crusher : MonoBehaviour
     public AudioClip crushSFX;
     private AudioSource audioSource;
 
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Crusher"))
