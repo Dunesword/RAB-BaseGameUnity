0f3ae58efcd257c2fcf654f3c0f415833ba13aab
Dunesword <157728274+Dunesword@users.noreply.github.com>
2026-06-08 22:22:28 -0400

    TotalTime now displays

--- Assets/Scripts/DisplayTimes.cs
+++ Assets/Scripts/DisplayTimes.cs
@@ -6,15 +6,19 @@ public class DisplayTimes : MonoBehaviour
 
     public TMP_Text[] levelTimes;
     public TMP_Text[] bestLevelTimes;
+    public TMP_Text totalTimeText;
+    private float totalTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        totalTime = 0f;
         for (int i = 1; i <= levelTimes.Length; i++)
         {
             if (PlayerPrefs.HasKey("LatestTimeLevel" + i))
             {
-                levelTimes[i].text = levelTimes[i].text.Substring(0, levelTimes[i].text.IndexOf(':') + 2) + TimerToString(PlayerPrefs.GetFloat("LatestTimeLevel" + i));
+                levelTimes[i].text = TimerToString(PlayerPrefs.GetFloat("LatestTimeLevel" + i));
+                totalTime += PlayerPrefs.GetFloat("LatestTimeLevel" + i);
             }
 
             if (PlayerPrefs.HasKey("BestTimeLevel" + i))
@@ -22,6 +26,7 @@ public class DisplayTimes : MonoBehaviour
                 bestLevelTimes[i].text = bestLevelTimes[i].text.Substring(0, bestLevelTimes[i].text.IndexOf(':') + 2) + TimerToString(PlayerPrefs.GetFloat("BestTimeLevel" + i));
             }
         }
+        totalTimeText.text = totalTimeText.text.Substring(0, totalTimeText.text.IndexOf(':') + 2) + TimerToString(totalTime);
     }
 
     string TimerToString(float time)
