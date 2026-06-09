0009452a5efa0b4b3ae9e679537cd10791ccb423
JennalynS <jstanbro1@my.waketech.edu>
2026-06-08 20:13:58 -0400

    Main Menu Transition script and animations added

---
+++ Assets/Scripts/MainMenuTransitionController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuTransitionController : MonoBehaviour
+{
+    public Animator transitionAnimator;
+    public GameObject transitionImage;
+    AudioSource audioSource;
+    public AudioClip continueSound;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transitionImage.SetActive(false);
+    }
+
+    public void OnClickStart()
+    {
+        transitionImage.SetActive(true); // On press start the fade in image with be set active and thefade in anim will play
+    }
+
+    void OnSpaceBarPress()
+    {
+        transitionAnimator.SetTrigger("SpaceBarPress");  // sets fade out anim trigger
+        audioSource.clip = continueSound; 
+        audioSource.Play(); // Plays transition sound
+        Invoke("SceneLoad", 1.5f); // loads scene after text has faded out
+
+    }
+
+    void SceneLoad()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transitionImage) // checks if transition image is set active (I think)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                OnSpaceBarPress(); // if player presses space and image is active, this will be called
+            }
+        }
+    }
+}
